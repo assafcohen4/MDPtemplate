92233c901f611a1a4613fcc964de6f89cb1edf09
assafcohen4 <assafcohen4@gmail.com>
2022-01-11 19:23:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,121 @@
+
+def value_iteration():
+    # Initialize Markov Decision Process model
+
+    washroom, kitchen, bedroom, diningroom, under_attack, dead = 0, 1, 2, 3, 4, 5
+    H, V, S, D, SD = 0, 1, 2, 3, 4
+    states = (washroom, kitchen, bedroom, diningroom, under_attack, dead)  # states (tiles)
+    actions = (H, V, S, D, SD)  # actions (0=left, 1=right)
+    rewards = [4, 10, 0, 2, -50, 0]  # Direct rewards per state
+    gamma = 0.5  # discount factor
+    # Transition probabilities per state-action pair
+    probs = [
+        [[0, 0, 0.75, 0, 0], [0.6, 0.4, 0, 0, 0], [0.4, 0.6, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0.25, 0, 0], [0, 0, 0, 0, 0]],
+        [[0.6, 0.4, 0, 0, 0], [0, 0, 0.75, 0, 0], [0, 0, 0, 0, 0], [0.4, 0.6, 0, 0, 0], [0, 0, 0.25, 0, 0], [0, 0, 0, 0, 0]],
+        [[0.4, 0.6, 0, 0, 0], [0, 0,0, 0, 0], [0, 0,0.75, 0, 0], [0.6, 0.4, 0, 0, 0], [0, 0, 0.25, 0, 0], [0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0], [0.6, 0.4, 0, 0, 0], [0.4, 0.6, 0, 0, 0], [0, 0, 0.75, 0, 0], [0, 0, 0.25, 0, 0], [0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 1, 1, 1, 1]],
+        [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 1, 1, 1, 1]]
+    ]
+
+    # Set value iteration parameters
+    max_iter = 5  # Maximum number of iterations
+    delta = 1e-400  # Error tolerance
+    V = [0, 0, 0, 0, 0, 0]  # Initialize values
+    pi = [None, None, None, None, None, None]  # Initialize policy
+
+
+    # Start value iteration
+    for i in range(max_iter):
+        max_diff = 0  # Initialize max difference
+        V_new = [0, 0, 0, 0, 0, 0]  # Initialize values
+        for s in states:
+            max_val = 0
+            for a in actions:
+
+                # Compute state value
+                val = rewards[s]  # Get direct reward
+                for s_next in states:
+                    val += probs[s][s_next][a] * (
+                        gamma * V[s_next]
+                    )  # Add discounted downstream values
+
+                # Store value best action so far
+                max_val = max(max_val, val)
+
+                # Update best policy
+                if V_new[s] < val:
+                    pi[s] = actions[a]  # Store action with highest value
+
+            V_new[s] = max_val  # Update value with highest value
+
+            # Update maximum difference
+            max_diff = max(max_diff, abs(V[s] - V_new[s]))
+
+        # Update value functions
+        V = V_new
+
+        # If diff smaller than threshold delta for all states, algorithm terminates
+        if max_diff < delta:
+            break
+
+    return pi
+
+
+def value_iteration_original():
+    # Initialize Markov Decision Process model
+    actions = (0, 1)  # actions (0=left, 1=right)
+    states = (0, 1, 2, 3, 4)  # states (tiles)
+    rewards = [-1, -1, 10, -1, -1]  # Direct rewards per state
+    gamma = 0.9  # discount factor
+    # Transition probabilities per state-action pair
+    probs = [
+        [[0.9, 0.1], [0.1, 0.9], [0, 0], [0, 0], [0, 0]],
+        [[0.9, 0.1], [0, 0], [0.1, 0.9], [0, 0], [0, 0]],
+        [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],  # Terminating state (all probs 0)
+        [[0, 0], [0, 0], [0.9, 0.1], [0, 0], [0.1, 0.9]],
+        [[0, 0], [0, 0], [0, 0], [0.9, 0.1], [0.1, 0.9]],
+    ]
+
+    # Set value iteration parameters
+    max_iter = 10000  # Maximum number of iterations
+    delta = 1e-400  # Error tolerance
+    V = [0, 0, 0, 0, 0]  # Initialize values
+    pi = [None, None, None, None, None]  # Initialize policy
+
+    # Start value iteration
+    for i in range(max_iter):
+        max_diff = 0  # Initialize max difference
+        V_new = [0, 0, 0, 0, 0]  # Initialize values
+        for s in states:
+            max_val = 0
+            for a in actions:
+
+                # Compute state value
+                val = rewards[s]  # Get direct reward
+                for s_next in states:
+                    val += probs[s][s_next][a] * (
+                            gamma * V[s_next]
+                    )  # Add discounted downstream values
+
+                # Store value best action so far
+                max_val = max(max_val, val)
+
+                # Update best policy
+                if V_new[s] < val:
+                    pi[s] = actions[a]  # Store action with highest value
+
+            V_new[s] = max_val  # Update value with highest value
+
+            # Update maximum difference
+            max_diff = max(max_diff, abs(V[s] - V_new[s]))
+
+        # Update value functions
+        V = V_new
+
+        # If diff smaller than threshold delta for all states, algorithm terminates
+        if max_diff < delta:
+            break
+    return pi
+if __name__ == '__main__':
+  print(value_iteration())
